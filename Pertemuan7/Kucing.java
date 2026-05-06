ea6056a3378d4aee6dfeeacab282f88fc7fbb744
Aprillia Abel Cleodora <aprilliaabl17@gmail.com>
2026-05-06 22:17:45 +0700

    re-add Pertemuan7

---
+++ Pertemuan7/Kucing.java
@@ -0,0 +1,14 @@
+/*
+Nama File : Kucing.java
+*/
+
+class Kucing extends Anabul {
+
+    public void suara() {
+        System.out.println("Meong");
+    }
+
+    public void gerak() {
+        System.out.println("Melata");
+    }
+}
